5bdd622ff865c81a9b87004ddd4f384ce389b196
mpl <mathieu.lonjaret@gmail.com>
2013-08-22 22:34:41 +0200

    all working on linux, now to test again on windows

--- backup-now.go
+++ backup-now.go
@@ -3,25 +3,27 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"encoding/json"
 	"fmt"
+	"encoding/json"
 	"log"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 )
 
 const (
 	configFile = "config.txt"
-	//	camputBin  = "camput.exe"
-	camputBin    = "camput"
+//	camputBin  = "camput.exe"
+	camputBin  = "camput"
 	camputConfig = "client-config.json"
+	blobRootLog = "log.txt"
 )
 
 var user, password, server string
 
-type simpleConfig struct {
-	Auth   string `json:"auth"`
+type simpleConfig struct{
+	Auth string `json:"auth"`
 	Server string `json:"server"`
 }
 
@@ -37,7 +39,7 @@ func genCamputConfig() {
 	}
 	defer f.Close()
 	conf := simpleConfig{
-		Auth:   fmt.Sprintf("userpass:%v:%v", user, password),
+		Auth: fmt.Sprintf("userpass:%v:%v", user, password),
 		Server: server,
 	}
 	jsonEncoder := json.NewEncoder(f)
@@ -46,11 +48,18 @@ func genCamputConfig() {
 	}
 }
 
+var blobRefRgx = regexp.MustCompile(`^sha1+-[a-f0-9]+$`)
+
 func dirBlobRef(b *bytes.Buffer) string {
-	return b.String()
+	br := strings.TrimSuffix(b.String(), "\n")
+	if !blobRefRgx.MatchString(br) {
+		log.Fatalf("Failed to get blobref from camput's output, got %v", br)
+	}
+	return br
 }
 
 func main() {
+	// Sanity checks
 	if _, err := os.Stat(configFile); err != nil {
 		log.Fatalf("Please provide a \"%v\" file in the same directory, containing the paths to the directories to backup (one per line).", configFile)
 	}
@@ -58,12 +67,12 @@ func main() {
 		log.Fatalf("Please place %v in the same directory as this program.", camputBin)
 	}
 
+	// Scan the config file
 	f, err := os.Open(configFile)
 	if err != nil {
 		log.Fatalf("Failed to open %v: %v", configFile, err)
 	}
 	defer f.Close()
-
 	var dirs []string
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
@@ -83,32 +92,42 @@ func main() {
 		log.Fatalf("Failed to read %v: %v", configFile, err)
 	}
 
+	// Create the camput config file
 	genCamputConfig()
 
+	// Setup the output
 	var output bytes.Buffer
-	commonArgs := []string{"file"}
+	lw, err := os.OpenFile(blobRootLog, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0600)
+	if err != nil {
+		log.Fatalf("Could not open %v: %v", blobRootLog, err)
+	}
+	defer lw.Close()
+	logger := log.New(lw, "", log.LstdFlags)
+
+	// Setup the command(s)
 	configDir, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("Failed to get current working directory: %v", err)
 	}
 	env := os.Environ()
 	env = append(env, "CAMLI_CONFIG_DIR="+configDir)
+	commonArgs := []string{"file"}
+
 	for _, dir := range dirs {
 		args := append(commonArgs, dir)
 		cmd := exec.Command(camputBin, args...)
 		cmd.Env = env
 		cmd.Stdout = &output
-		//		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		log.Printf("Now running %v %v", camputBin, args)
 		if err := cmd.Run(); err != nil {
 			log.Fatalf("Failed to upload dir %v: %v", dir, err)
 		}
-		// We rely on the last line of the camput output to be the
-		// bloref for the root node of the dir. Sane enough?
-		println("WAT")
-		println(output.String())
-		//		dirBlobRef(&output)
+		// We rely on the camput output to be the bloref for
+		// the root node of the dir. Sane enough?
+		br := dirBlobRef(&output)
+		logger.Printf("%v: %v", dir, br)
+		output.Reset()
 	}
 	log.Print("Upload successful")
 }
